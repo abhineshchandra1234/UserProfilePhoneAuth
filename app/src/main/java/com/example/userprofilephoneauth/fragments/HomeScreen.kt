a1ebe1652e0c059dfb7d1527f72c5c6703181444
abhinesh chandra <abhineshchandra1234@gmail.com>
2021-04-01 09:57:14 +0530

    remove reduntant code of frags

---
+++ app/src/main/java/com/example/userprofilephoneauth/fragments/HomeScreen.kt
@@ -0,0 +1,13 @@
+package com.example.userprofilephoneauth.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.userprofilephoneauth.R
+
+
+class HomeScreen : Fragment(R.layout.fragment_home_screen) {
+
+}
---
+++ app/src/main/java/com/example/userprofilephoneauth/fragments/UserDetails.kt
@@ -0,0 +1,13 @@
+package com.example.userprofilephoneauth.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.userprofilephoneauth.R
+
+
+class UserDetails : Fragment(R.layout.fragment_user_details) {
+
+}
