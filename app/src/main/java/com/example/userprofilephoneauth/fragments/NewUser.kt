dfc168d31f535dda1a6f42ad2315bfe97d84af87
abhinesh chandra <abhineshchandra1234@gmail.com>
2021-04-03 11:04:00 +0530

    added enter no for new user && func in newuser frag

--- app/src/main/java/com/example/userprofilephoneauth/fragments/NewUser.kt
+++ app/src/main/java/com/example/userprofilephoneauth/fragments/NewUser.kt
@@ -38,16 +38,9 @@ class NewUser : Fragment(R.layout.fragment_new_user) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding = FragmentNewUserBinding.bind(view)
         auth=FirebaseAuth.getInstance()
-        val currentUser = auth.currentUser
-        number = currentUser?.phoneNumber.toString()
-        Toast.makeText(context, "phone no is ${currentUser?.phoneNumber}", Toast.LENGTH_SHORT).show()
-        Log.d("Main", "current user phone no is ${currentUser?.phoneNumber}")
         userViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
         viewModel = ViewModelProvider(this).get(TestViewModel::class.java)
 
-        if(currentUser != null) {
-            checkUserStatus()
-        }
 
 
 
@@ -59,52 +52,9 @@ class NewUser : Fragment(R.layout.fragment_new_user) {
             auth.signOut()
             findNavController().navigate(R.id.action_newUser_to_homeScreen)
         }
-//        addUser()
-//        checkNumberStatus()
-
-//        parseList()
-    }
-
-    private fun checkUserStatus() {
-        Log.d("Main", "checkUserStatus is called")
-        val job = GlobalScope.launch {
-            if (userViewModel.checkNumber(number)) {
-                userExist = "true"
-                Log.d("Main", "this will print first")
-            }
-        }
-
-        runBlocking {
-            job.join()	//it will block the main thread until the job is finished then continue
-            job.cancel() //it is used to cancel job
-            if (userExist == "true") {
-                Log.d("Main", "userExist value is $userExist")
-                findNavController().navigate(R.id.action_newUser_to_userDetails)
-            }
-            Log.d("Main", "this will print later")
-        }
-
-
-//        GlobalScope.launch(Dispatchers.IO) {
-//            val time = measureTimeMillis {
-//                val answer1 = async { networkCall1() } //it will not return job but deffered string
-//                val answer2 = async { networkCall2() }	//async is used when coroutine return something, it is more efficient than launch
-//                Log.d(TAG, "Answer1 is ${answer1.await()}")	//it will wait for value then print it
-//                Log.d(TAG, "Answer2 is ${answer2.await()}")
-//            }
-//            Log.d(TAG, "Requests took ${time/1000} secs")
-//        }
-//        Log.d("Main", "userExist value is $userExist")
-
-
 
     }
 
-
-
-    private fun parseList() {
-
-    }
 
 //    private fun addUser() {
 //        for (i in 1..5) {
@@ -122,10 +72,9 @@ class NewUser : Fragment(R.layout.fragment_new_user) {
     private fun registerNewUser() {
         val name = binding.etEnterName.text.toString().trim()
         val email = binding.etEnterEmail.text.toString().trim()
-        val currentUser = auth.currentUser
-        val userNumber = currentUser?.phoneNumber
+        val number = binding.etEnterNumber.text.toString().trim()
         if (!(name.isEmpty() || email.isEmpty())) {
-            val user = userNumber?.let { User(it,name,email) }
+            val user = User(number,name,email)
             if (user != null) {
                 userViewModel.addUser(user)
             }
